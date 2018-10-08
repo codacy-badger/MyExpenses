d20b45e825db960ee80a1a072ada49634f955360
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2018-10-08 22:21:31 +0100

    Automapper

---
+++ Domain/Source/AutoMapperDomainConfiguration.cs
@@ -0,0 +1,26 @@
+﻿/* 
+*   Project: MyExpenses
+*   Author: Luiz Felipe Machado da Silva
+*   Github: http://github.com/lfmachadodasilva/MyExpenses
+*/
+
+namespace MyExpenses.Domain
+{
+    using AutoMapper;
+    using MyExpenses.Domains;
+
+    public class AutoMapperDomainConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Reset();
+            Mapper.Initialize(
+                cfg =>
+                {
+                    cfg.CreateMap<ExpenseDomain, ExpenseDomain>();
+                    cfg.CreateMap<LabelDomain, LabelDomain>();
+                    cfg.CreateMap<PaymentDomain, PaymentDomain>();
+                });
+        }
+    }
+}
