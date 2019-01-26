fed6798a6b073eed77b8266e62f733c67860c167
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2019-01-26 10:48:57 +0000

    Get all labels by group, month and year #192

--- src/lfmachadodasilva.MyExpenses.Core/Models/PaymentModel.cs
+++ src/lfmachadodasilva.MyExpenses.Core/Models/PaymentModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lfmachadodasilva.MyExpenses.Core.Models
@@ -6,6 +7,7 @@ namespace lfmachadodasilva.MyExpenses.Core.Models
     [Table(ModelUtility.PaymentTable)]
     public class PaymentModel : ModelBase
     {
+        [Required]
         public string Name { get; set; }
 
         public IEnumerable<ExpenseModel> Expenses { get; set; }
